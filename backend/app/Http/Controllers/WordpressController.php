43074a5cfbc7b3c0f775e398908180d4cb8f8213
Laurena Fayad <laurenafayad@gmail.com>
2022-04-28 08:00:25 +0300

    Added function to fetch plugin data from WP API and filter only the ratings, version, and installations

--- backend/app/Http/Controllers/WordpressController.php
+++ backend/app/Http/Controllers/WordpressController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Http;
+
 class WordpressController extends Controller
 {
-    //
+    public function getPluginDetails(){
+        $plugin_info = Http::get('https://api.wordpress.org/plugins/info/1.0/simply-schedule-appointments.json');
+        $json_result = json_decode($plugin_info);
+        $installations = $json_result->downloaded;
+        $version = $json_result->version;
+        $ratings = $json_result->ratings;
+        $five_star_ratings = $ratings->{5};
+        $other_ratings = $ratings->{4} + $ratings->{3} + $ratings->{2} + $ratings->{1};
+        $date = date('Y-m-d');
+
+        return [$five_star_ratings,$other_ratings,$installations,$version, $date];
+    }
 }
